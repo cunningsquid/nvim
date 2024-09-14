6ed3c04cba96d1b0f3034af25487eaa3a42a3928
cunningsquid <cunningsquid618@gmail.com>
2024-09-14 17:39:15 +0200

    Initial commit 2

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {}),
+	vim.keymap.set("n", "<C-m>", ":Neotree filesystem close<CR>", {}),
+}
