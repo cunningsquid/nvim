78f682aaf66a86f07cac7e649f1d259c58ac27ac
cunningsquid <cunningsquid618@gmail.com>
2025-09-25 21:42:05 +0200

    add better line numbering

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.wo.number = true
+vim.cmd("set number relativenumber")
 
 local mason_path = vim.fn.stdpath("data") .. "/mason/bin"
 vim.env.PATH = mason_path .. ":" .. vim.env.PATH
